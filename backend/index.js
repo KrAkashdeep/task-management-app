3b23905ea1159808bd58464baeaa235793c36428
KrAkashdeep <akashdeep93140@gmail.com>
2025-01-18 22:23:02 +0530

    add files

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ const cors = require("cors");
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
