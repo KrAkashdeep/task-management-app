89808e3fa01cf1063107c715b18977cc56342637
Kumar Akashdeep <110689554+KrAkashdeep@users.noreply.github.com>
2025-01-18 21:29:46 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,4 +2,4 @@ import { toast } from "react-toastify";
 export const notify = (message, type) => {
   toast[type](message);
 };
-export const API_URL = "https://task-management-app-beta-nine.vercel.app/";
+export const API_URL = "https://task-management-app-beta-nine.vercel.app";
